f06bdeaf34d3384e9d469f2d9ba5b1ed7a5ede3a
DESKTOP-RQ6DFL3\Administrator <ssongs77@gmail.com>
2017-08-09 16:19:10 +0900

    3-3 완료

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -8,28 +8,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
 import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user : " + user);
 		//users.add(user);
 		userRepository.save(user);
-		return "redirect:list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model){
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";
 	}
 }
