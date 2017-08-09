d63b204a385d5f25e7d53b309e1e224c907c5b1d
DESKTOP-RQ6DFL3\Administrator <ssongs77@gmail.com>
2017-08-09 20:13:13 +0900

    3-5 사용자 정보 수정 적용

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,21 +18,40 @@ import net.slipp.domain.UserRepository;
 @RequestMapping("/users")
 public class UserController {
 	private List<User> users = new ArrayList<User>();
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+
 	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user : " + user);
-		//users.add(user);
+		// users.add(user);
 		userRepository.save(user);
 		return "redirect:/users";
 	}
-	
+
 	@GetMapping("")
-	public String list(Model model){
+	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
+	}
+
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("user", userRepository.findOne(id));
+		return "/user/updateForm";
+	}
+
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
